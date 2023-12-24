66cbd068b7442aed51c1a423993c10877142d199
Ruslanllll <innaryslanllll@gmail.com>
2023-12-24 23:54:32 +0200

    first commit

---
+++ src/main/java/com/changed/CangedCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.changed;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CangedCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
